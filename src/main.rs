afe6936de5ba53f1d26dc47f01e8d96b1e296375
Lukas Strnad <strnad.lukas@proton.me>
2023-11-30 22:13:34 +0100

    Fake error

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use axum::{routing::get, Router};
+use axum::Router;
+use axum::{http::StatusCode, routing::get};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn server_error() -> StatusCode {
+    StatusCode::INTERNAL_SERVER_ERROR
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/error", get(server_error));
 
     Ok(router.into())
 }
