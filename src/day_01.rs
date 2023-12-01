367563572b73e068ffe774a19417670aab6d8e80
Lukas Strnad <strnad.lukas@proton.me>
2023-12-01 22:44:35 +0100

    Task2 using FP principles

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,9 +1,11 @@
-use axum::{extract::Path, http::StatusCode, response::IntoResponse};
+use axum::{extract::Path, response::IntoResponse};
 
 pub async fn day01(Path(nums): Path<String>) -> impl IntoResponse {
-    let mut sum = 0;
-    nums.split('/').for_each(|num| {
-        sum = sum ^ num.parse::<i32>().unwrap();
-    });
-    (StatusCode::OK, format!("{}", sum.pow(3)))
+    nums.split('/')
+        .map(|num| num.parse::<i32>().unwrap_or(0))
+        .reduce(|a, b| a ^ b)
+        .unwrap_or(0)
+        .pow(3)
+        .to_string()
+        .into_response()
 }
